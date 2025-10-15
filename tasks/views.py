ebfe1b7d82600e3fa88dafde3c97a5fd088bbfd3
Qwire <danulovitryak299@gmail.com>
2025-10-15 19:50:55 +0300

    add views and templates

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DetailView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 from tasks import models
 from tasks import forms
+from tasks.mixins import UserIsOwnerMixin
 
 class TaskListView(ListView):
     model = models.Task
@@ -21,17 +23,19 @@ class TasksCreateView(LoginRequiredMixin, CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
-class TasksUpdateView(LoginRequiredMixin, UpdateView):
+class TasksUpdateView(LoginRequiredMixin, UserIsOwnerMixin, UpdateView):
     model = models.Task
+    form_class = forms.TaskForm
     template_name = 'tasks/tasks_update.html'
     success_url = reverse_lazy("tasks-list")
+
 
 class TasksDetailView(DetailView):
     model = models.Task
     context_object_name = 'task'
     template_name = 'tasks/tasks_detail.html'
 
-class TaskDeleteView(LoginRequiredMixin, DetailView):
+class TaskDeleteView(LoginRequiredMixin, UserIsOwnerMixin, DeleteView):
     model = models.Task
     template_name = 'tasks/tasks_delete.html'
     success_url = reverse_lazy("tasks-list")
